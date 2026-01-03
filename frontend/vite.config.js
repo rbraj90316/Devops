d6067b35007eae753c31ec9dac20c22c8df79d4d
Rakesh Kumar <420rbraj@gmail.com>
2026-01-04 01:12:29 +0530

    Use Nginx to serve production React build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(),
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-  server: {
-  host: true,
-  allowedHosts: ['*']
-}
 
+export default defineConfig({
+  plugins: [tailwindcss(), react()],
 })
