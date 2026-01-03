5605a9117d655e4dda5a9fb5f98282cd20708bf4
Rakesh Kumar <420rbraj@gmail.com>
2026-01-03 23:01:20 +0530

    Initial commit: React + Node with Docker Compose

---
+++ README.md
@@ -0,0 +1,17 @@
+# Full-Stack DevOps Project
+
+## Tech Stack
+- React
+- Node.js
+- Docker
+- GitHub Actions
+- Render
+
+## DevOps Practices Used
+- Dockerized frontend and backend
+- CI/CD pipeline using GitHub Actions
+- Automated deployment
+- One-command local setup using Docker Compose
+
+## How to Run Locally
+docker-compose up
---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-3xl text-amber-400 p-4 bg-blue-700 rounded-4xl border-4'>Frontend running 🚀</h1>
+      <h1 className='text-3xl text-amber-400 p-4 bg-blue-700 rounded-4xl border-4'>Frontend running 🚀</h1>
+      pjhsd
+    </>
+  )
+}
+
+export default App
