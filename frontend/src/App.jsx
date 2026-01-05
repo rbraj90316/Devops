841386fb01d16ea584cfc8f59468b170c0f7ce44
Rakesh Kumar <420rbraj@gmail.com>
2026-01-05 20:43:32 +0530

    updated App.tsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <h1 className='text-3xl text-amber-400 p-4 bg-blue-700 rounded-4xl border-4'>Frontend running 🚀</h1>
       <h1 className='text-3xl text-amber-400 p-4 bg-blue-700 rounded-4xl border-4'>Frontend running 🚀</h1>
-      pjhsd
+      <p className='text-2xl text-cyan-500 bg-amber-950 border-2 rounded-2xl'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate repellat, aspernatur dignissimos nam nulla cumque iure quasi voluptatibus dolor sed? Provident dicta at libero. A magni dolores odit nisi laudantium.</p>
     </>
   )
 }
